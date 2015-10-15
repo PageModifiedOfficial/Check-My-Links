c11a1d8a7f4a4471ee794e8fe2d0d70826485029
Paul Livingstone <paul@ocodia.com>
2015-10-15 19:03:00 +0100

    Revert "Merge pull request #37 from kyleladd/KL/Install"
    This reverts commit f6060cae7800fc47ecece8f96e29d88e3cdb080e, reversing
    changes made to 003a0c240b319777e80d75f3cce9a3c575c13c48.

--- background.js
+++ background.js
@@ -44,16 +44,6 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     });
 });
 
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        chrome.tabs.create({url: "options.html?newinstall=yes"});
-    }
-    else if(details.reason == "update"){
-        var thisVersion = chrome.runtime.getManifest().version;
-        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-    }
-});
-
 function onRequest(request, sender, callback) {
     if (request.action == "check") {
         if (request.url) {
