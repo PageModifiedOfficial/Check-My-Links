3d0a17c788cbee2a1074592329e0e38a1b8058db
Mike Benner <refriedchicken@gmail.com>
2019-02-05 13:38:50 -0700

    adding google analytics

--- check.js
+++ check.js
@@ -8,83 +8,81 @@ chrome.extension.onMessage.addListener(
 
   function doStuff(request, sender) {
 
-  if(request.action == "initial"){
-    var rpBox;
-    var blacklist = request.options.blacklist;
-    blacklist = blacklist.split("\n");
-    var cacheType = request.options.cache;
-    var checkType = request.options.checkType;
-    var optURL = request.options.optionsURL;
+    if (request.action == "initial") {
+      var rpBox;
+      var blacklist = request.options.blacklist;
+      blacklist = blacklist.split("\n");
+      var cacheType = request.options.cache;
+      var checkType = request.options.checkType;
+      var optURL = request.options.optionsURL;
 
-    // Inject Styles and Elements for feedback report
-    createDisplay(optURL,cacheType,checkType);
+      // Inject Styles and Elements for feedback report
+      createDisplay(optURL, cacheType, checkType);
       // Gather links
-    var pageLinks = document.getElementsByTagName('a');
-        
-    log(pageLinks);
-    var totalvalid = pageLinks.length;
-    
-    for (var i = 0; i < pageLinks.length; i++){
-      var link = pageLinks[i];
-      var isValidLink = false;
-      isValidLink = isLinkValid(link,request,blacklist);
-      if(isValidLink === true){
-        queued += 1;
-        link.classList.add("CMY_Link");
-        checkURL(link, request.options);
+      var pageLinks = document.getElementsByTagName('a');
+
+      log(pageLinks);
+      var totalvalid = pageLinks.length;
+
+      for (var i = 0; i < pageLinks.length; i++) {
+        var link = pageLinks[i];
+        var isValidLink = false;
+        isValidLink = isLinkValid(link, request, blacklist);
+        if (isValidLink === true) {
+          queued += 1;
+          link.classList.add("CMY_Link");
+          checkURL(link, request.options);
+        } else {
+          totalvalid -= 1;
+        }
       }
-      else{
-        totalvalid -= 1;
-      }
-    }
-    rbAmt.innerHTML = "Links: " + totalvalid;
-    // When close element is clicked, hide UI
-    document.getElementById("CMY_RB_Close").onclick = function(){removeDOMElement("CMY_ReportBox");};
-    document.getElementById("CMY_RB_Export").onclick = function(){
-    var output = "";
-    var badLinks = document.getElementsByClassName("CMY_Invalid");
-    // Export csv string so it is accessible via excel
-    if(badLinks.length > 0){
-      output += "URL,OuterHTML\n";
-      for (i = 0; i < badLinks.length; i++) {
-        var outerHTML;
-        output += "\"";
-        output += badLinks[i].href;
-        output += "\",";
-        output += "\"";
-        outerHTML = badLinks[i].outerHTML.replace(/"/g, '""');
-        output += outerHTML;
-        output += "\"";
-        output += "\n";
-      }
-      output = output.rtrim(',');
-    }
-    else{
-      output = "No links to export";
-    }
-    console.log(output);
-    };
-    // Remove the event listener in the event this is run again without reloading
-    chrome.extension.onMessage.removeListener(doStuff);
+      rbAmt.innerHTML = "Links: " + totalvalid;
+      // When close element is clicked, hide UI
+      document.getElementById("CMY_RB_Close").onclick = function() { removeDOMElement("CMY_ReportBox"); };
+      document.getElementById("CMY_RB_Export").onclick = function() {
+        var output = "";
+        var badLinks = document.getElementsByClassName("CMY_Invalid");
+        // Export csv string so it is accessible via excel
+        if (badLinks.length > 0) {
+          output += "URL,OuterHTML\n";
+          for (i = 0; i < badLinks.length; i++) {
+            var outerHTML;
+            output += "\"";
+            output += badLinks[i].href;
+            output += "\",";
+            output += "\"";
+            outerHTML = badLinks[i].outerHTML.replace(/"/g, '""');
+            output += outerHTML;
+            output += "\"";
+            output += "\n";
+          }
+          output = output.rtrim(',');
+        } else {
+          output = "No links to export";
+        }
+        console.log(output);
+      };
+      // Remove the event listener in the event this is run again without reloading
+      chrome.extension.onMessage.removeListener(doStuff);
     }
     return true;
 
   });
-  // Send links to get checked via XHR
-  function checkURL(link,options) {
-    // For empty href or no attribute href elements
-    var checkElement = create("a", {
-      href: link.href
-    });
-    chrome.extension.sendMessage({"action": "check", "url": checkElement.href},
-    function (response) {
+// Send links to get checked via XHR
+function checkURL(link, options) {
+  // For empty href or no attribute href elements
+  var checkElement = create("a", {
+    href: link.href
+  });
+  chrome.extension.sendMessage({ "action": "check", "url": checkElement.href },
+    function(response) {
       // Assess Warnings
       var warnings = [];
-      warnings = getTrailingHashWarning(options,link,warnings);
-      warnings = getParseDOMWarning(options,link.href,response,warnings);
+      warnings = getTrailingHashWarning(options, link, warnings);
+      warnings = getParseDOMWarning(options, link.href, response, warnings);
       // Pass in the outerHTML, the href attributes defaults to the current page if left empty
-      warnings = getEmptyLinkWarning(options,link.outerHTML,warnings);
-      warnings = getNoHrefLinkWarning(options,link,warnings);      
-      updateDisplay(link,warnings,response.status);
+      warnings = getEmptyLinkWarning(options, link.outerHTML, warnings);
+      warnings = getNoHrefLinkWarning(options, link, warnings);
+      updateDisplay(link, warnings, response.status);
     });
-  }
+}
--- options.js
+++ options.js
@@ -1,44 +1,54 @@
-// Check My Links by Paul Livingstone
-// @ocodia
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-98457349-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
 
 function loadOptions() {
-  
-  var bkg = chrome.runtime.getBackgroundPage(function(bkg){
-   var options = bkg.getOptions();
 
-    if(options.cache == 'true'){
+  var bkg = chrome.runtime.getBackgroundPage(function(bkg) {
+    var options = bkg.getOptions();
+
+    if (options.cache == 'true') {
       document.getElementById("cache").checked = true;
     }
 
-    if(options.noFollow == 'true'){
+    if (options.noFollow == 'true') {
       document.getElementById("noFollow").checked = true;
     }
 
-    if(options.parseDOM == 'true'){
+    if (options.parseDOM == 'true') {
       document.getElementById("parseDOM").checked = true;
     }
 
-    if(options.trailingHash == 'true'){
+    if (options.trailingHash == 'true') {
       document.getElementById("trailingHash").checked = true;
     }
 
-    if(options.emptyLink == 'true'){
+    if (options.emptyLink == 'true') {
       document.getElementById("emptyLink").checked = true;
     }
 
-    if(options.noHrefAttr == 'true'){
+    if (options.noHrefAttr == 'true') {
       document.getElementById("noHrefAttr").checked = true;
     }
-    if(options.autoCheck == 'true'){
+    if (options.autoCheck == 'true') {
       document.getElementById("autoCheck").checked = true;
     }
 
     document.getElementById("blacklistEntries").value = options.blacklist.split(" ");
     var requestType = document.getElementById("requestType");
-   
+
     for (var i = 0; i < requestType.children.length; i++) {
       var requestTypechild = requestType.children[i];
-        if (requestTypechild.value == options.checkType) {
+      if (requestTypechild.value == options.checkType) {
         requestTypechild.selected = "true";
         break;
       }
@@ -47,7 +57,7 @@ function loadOptions() {
 }
 
 function saveOptions() {
-  var bkg = chrome.runtime.getBackgroundPage(function(bkg){
+  var bkg = chrome.runtime.getBackgroundPage(function(bkg) {
     var blacklistEntries = document.getElementById("blacklistEntries");
     var requestType = document.getElementById("requestType");
 
@@ -55,13 +65,13 @@ function saveOptions() {
     bkg.setItem("blacklist", blacklistEntries.value);
     bkg.setItem("checkType", requestType.children[requestType.selectedIndex].value);
 
-    if(document.getElementById("cache").checked){bkg.setItem("cache", 'true');}else{bkg.setItem("cache", 'false');}
-    if(document.getElementById("noFollow").checked){bkg.setItem("noFollow", 'true');}else{bkg.setItem("noFollow", 'false');}
-    if(document.getElementById("parseDOM").checked){bkg.setItem("parseDOM", 'true');}else{bkg.setItem("parseDOM", 'false');}
-    if(document.getElementById("trailingHash").checked){bkg.setItem("trailingHash", 'true');}else{bkg.setItem("trailingHash", 'false');}
-    if(document.getElementById("emptyLink").checked){bkg.setItem("emptyLink", 'true');}else{bkg.setItem("emptyLink", 'false');}
-    if(document.getElementById("noHrefAttr").checked){bkg.setItem("noHrefAttr", 'true');}else{bkg.setItem("noHrefAttr", 'false');}
-    if(document.getElementById("autoCheck").checked){bkg.setItem("autoCheck", 'true');}else{bkg.setItem("autoCheck", 'false');}
+    if (document.getElementById("cache").checked) { bkg.setItem("cache", 'true'); } else { bkg.setItem("cache", 'false'); }
+    if (document.getElementById("noFollow").checked) { bkg.setItem("noFollow", 'true'); } else { bkg.setItem("noFollow", 'false'); }
+    if (document.getElementById("parseDOM").checked) { bkg.setItem("parseDOM", 'true'); } else { bkg.setItem("parseDOM", 'false'); }
+    if (document.getElementById("trailingHash").checked) { bkg.setItem("trailingHash", 'true'); } else { bkg.setItem("trailingHash", 'false'); }
+    if (document.getElementById("emptyLink").checked) { bkg.setItem("emptyLink", 'true'); } else { bkg.setItem("emptyLink", 'false'); }
+    if (document.getElementById("noHrefAttr").checked) { bkg.setItem("noHrefAttr", 'true'); } else { bkg.setItem("noHrefAttr", 'false'); }
+    if (document.getElementById("autoCheck").checked) { bkg.setItem("autoCheck", 'true'); } else { bkg.setItem("autoCheck", 'false'); }
 
     document.getElementById("msg").style.visibility = "visible";
     setTimeout(function() {
@@ -70,7 +80,7 @@ function saveOptions() {
   });
 }
 
-function deleteObjectStore(){
+function deleteObjectStore() {
   indexedDBHelper.deleteObjectStore();
   console.log("Cleared IndexedDB Datastore");
   document.getElementById("msgCache").style.visibility = "visible";
